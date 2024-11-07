d4e080d7489b3801bc72eea0eebdfd98918ddd15
Benjamin Piouffle <benjamin@opencollective.com>
2024-11-07 11:05:14 +0100

    update eslint config for v9

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import openCollectiveConfig from 'eslint-config-opencollective/eslint.config.cjs'; // eslint-disable-line n/no-unpublished-import
+import globals from 'globals'; // eslint-disable-line n/no-unpublished-import
+
+export default [
+  ...openCollectiveConfig,
+  {
+    ignores: ['**/node_modules/', '**/dist/', '**/coverage/', '**/.nyc_output', '**/.vscode', '**/.history'],
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.jest },
+    },
+    rules: {
+      'no-console': 'warn',
+      'node/no-missing-import': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
+];
